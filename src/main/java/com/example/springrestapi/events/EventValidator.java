a1792f5b3ddaa39bdbcd121b7dcd2058a23fe0c6
DongHunKim <dh.kim@bfactory.inc>
2021-07-09 16:08:18 +0900

    feat : evnet 관련 추가
    - 이벤트 검증클래스

---
+++ src/main/java/com/example/springrestapi/events/EventValidator.java
@@ -0,0 +1,23 @@
+package com.example.springrestapi.events;
+
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+
+import java.time.LocalDateTime;
+
+@Component
+public class EventValidator {
+
+    public void validate(EventDto eventDto, Errors errors) {
+        if(eventDto.getBasePrice() > eventDto.getMaxPrice() && eventDto.getMaxPrice() != 0) {
+            errors.reject("wrongPrices", "wrong price error");
+        }
+
+        LocalDateTime endEventDateTime = eventDto.getEndEventDateTime();
+        if(endEventDateTime.isBefore(eventDto.getBeginEventDateTime()) ||
+                endEventDateTime.isBefore(eventDto.getCloseEnrollmentDateTime()) ||
+                endEventDateTime.isBefore(eventDto.getBeginEnrollmentDateTime())) {
+            errors.rejectValue("endEventDateTime", "wrongValue", "Wrong Date Time");
+        }
+    }
+}
