bc5283ff4162143965ab377791e1968fa5277417
DongHunKim <dh.kim@bfactory.inc>
2021-07-09 15:36:18 +0900

    feat : dto 추가

---
+++ src/main/java/com/example/springrestapi/events/EventDto.java
@@ -0,0 +1,39 @@
+package com.example.springrestapi.events;
+
+import com.sun.istack.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class EventDto {
+
+    @NotEmpty
+    private String name;
+    @NotEmpty
+    private String description;
+    @NotNull
+    private LocalDateTime beginEnrollmentDateTime;
+    @NotNull
+    private LocalDateTime closeEnrollmentDateTime;
+    @NotNull
+    private LocalDateTime beginEventDateTime;
+    @NotNull
+    private LocalDateTime endEventDateTime;
+    private String location; // (optional)
+    @Min(0)
+    private int basePrice; // (optional)
+    @Min(0)
+    private int maxPrice;  // (optional)
+    @Min(0)
+    private int limitOfEnrollment;
+
+
+}
