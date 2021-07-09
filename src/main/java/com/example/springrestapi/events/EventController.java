8b88a278beddc3d3430c2888921540c347e34c03
DongHunKim <dh.kim@bfactory.inc>
2021-07-10 00:23:39 +0900

    feat : evnet 관련 추가
    - 컨트롤러 추가

---
+++ src/main/java/com/example/springrestapi/events/EventController.java
@@ -0,0 +1,57 @@
+package com.example.springrestapi.events;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.hateoas.MediaTypes;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
+import java.net.URI;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+
+@Controller
+@RequestMapping(value = "/api/events", produces = MediaTypes.HAL_JSON_VALUE)
+public class EventController {
+
+    @Autowired
+    EventRepository eventRepository;
+
+    @Autowired
+    ModelMapper modelMapper;
+
+    @Autowired
+    EventValidator eventValidator;
+
+
+    @PostMapping
+    public ResponseEntity createEvent(@RequestBody @Valid EventDto eventDto, Errors errors) {
+        if(errors.hasErrors()) {
+            return ResponseEntity.badRequest().body(errors);
+        }
+
+        eventValidator.validate(eventDto, errors);
+        if(errors.hasErrors()) {
+            return ResponseEntity.badRequest().body(errors);
+        }
+
+        Event event = modelMapper.map(eventDto, Event.class);
+        event.update();
+        Event newEvent = this.eventRepository.save(event);
+
+        WebMvcLinkBuilder selfLinkBuilder = linkTo(EventController.class).slash(newEvent.getId());
+        URI createdURI = selfLinkBuilder.toUri();
+        EventResource eventResource = new EventResource(newEvent);
+        eventResource.add(linkTo(EventController.class).withRel("query-events"));
+        eventResource.add(linkTo(EventController.class).withRel("update-events"));
+        return ResponseEntity.created(createdURI).body(eventResource);
+    }
+
+}
