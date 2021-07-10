b21b643c6a18564c96bf3668adf9c346265a17c7
DongHunKim <dh.kim@bfactory.inc>
2021-07-10 09:29:38 +0900

    feat : 빈등록추가

--- src/main/java/com/example/springrestapi/ApplicationConfig.java
+++ src/main/java/com/example/springrestapi/ApplicationConfig.java
@@ -1,6 +1,8 @@
 package com.example.springrestapi;
 
 import org.modelmapper.ModelMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -8,7 +10,11 @@ import org.springframework.context.annotation.Configuration;
 public class ApplicationConfig {
     @Bean
     public ModelMapper modelMapper() {
-        ModelMapper modelMapper = new ModelMapper();
-        return modelMapper;
+        return new ModelMapper();
+    }
+
+    @Bean
+    public Logger logger() {
+        return LoggerFactory.getLogger("myApp");
     }
 }
